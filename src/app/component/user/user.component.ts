fc31569e9035e5c2e7f5a91ac5e7223687496afe
jayson911 <jayson406@gmail.com>
2020-10-04 21:37:02 +0300

    var 3 end

--- src/app/component/user/user.component.ts
+++ src/app/component/user/user.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {User} from '../../models/user';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -11,9 +12,17 @@ export class UserComponent implements OnInit {
   @Input()
   user: User;
 
-  constructor() { }
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute) {
+    console.log(this.user);
+  }
 
   ngOnInit(): void {
+  }
+  showUser(user: User): void {
+    console.log(this.user);
+
+    this.router.navigate(['details', this.user.id], {state : {xxx: user}, relativeTo : this.activatedRoute});
   }
 
 }
